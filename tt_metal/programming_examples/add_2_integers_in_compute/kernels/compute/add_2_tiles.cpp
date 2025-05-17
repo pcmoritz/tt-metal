4d21f8adf7547c82e9935ff2daf34defdc310523
Philipp Moritz <pcmoritz@gmail.com>
2025-05-17 16:43:30 -0700

    update

--- tt_metal/programming_examples/add_2_integers_in_compute/kernels/compute/add_2_tiles.cpp
+++ tt_metal/programming_examples/add_2_integers_in_compute/kernels/compute/add_2_tiles.cpp
@@ -6,6 +6,7 @@
 #include "compute_kernel_api/common.h"
 #include "compute_kernel_api/eltwise_binary.h"
 #include "compute_kernel_api/tile_move_copy.h"
+#include "compute_kernel_api/eltwise_unary/eltwise_unary.h"
 #include "compute_kernel_api/eltwise_unary/typecast.h"
 #include "compute_kernel_api/eltwise_unary/sfpu_split_includes.h"
 
